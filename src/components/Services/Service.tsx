f1932e686ef9e0478789d29ac14250fdfcf3f5d2
Mohammed Faizan Ahmed <faizanzaid789@gmail.com>
2023-05-23 18:46:36 +0530

    ui: added linear gradient to service

--- src/components/Services/Service.tsx
+++ src/components/Services/Service.tsx
@@ -7,10 +7,10 @@ interface ServiceProps{
 const Service:React.FC<ServiceProps> = ({name, banner, off}):JSX.Element => {
   return (
     <div className="w-44 rounded-lg shadow-md relative flex-col flex bg-white" >
-      <img src={banner} alt={name} className="w-full  object-cover rounded-t-lg flex-1"/>
+      <img src={banner} alt={name} className="w-full  object-cover rounded-t-lg flex-1 "/>
       <div className="absolute top-0 right-0 bg-green-600 rounded-sm px-[6px] py-[2px] text-white">{off}% off</div>
-      <div className="text-center text-sm md:text-lg font-bold p-1">{name}</div>
-      
+      <div className="text-center text-sm md:text-lg text-white z-10  font-bold py-2">{name}</div>
+      <div className="bg-gradient-to-t from-black to-transparent absolute bottom-0 h-36 w-full"/>
 
     </div>
   )
